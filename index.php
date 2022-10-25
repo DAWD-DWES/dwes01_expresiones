62c727cfc49fe65b8b54d895bc60fb386c17216d
Iván <jivanrod@gmail.com>
2022-10-25 18:19:47 +0200

    update

--- index.php
+++ index.php
@@ -11,7 +11,22 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        $a = 5;
+        $b = 10;
+        $c = 15;
+        $texto = "mundo";
+
+        echo '$a + $b * $c / ($a - 3) : ', var_dump($a + $b * $c / ($a - 3)), "</br>";
+        echo 'true && false : ', var_dump(true && false), "</br>";
+        echo 'true and false : ', var_dump(true and false), "</br>";
+        echo '$resultado = $a++ : ', var_dump($resultado = ++$a), "</br>";
+        echo '$saludo .= "Hola " : ', var_dump($texto .= "Hola "), "</br>";
+        echo '$x = ($y = 11)+9 : ', var_dump($x = ($y = 11) + 9), "</br>";
+        echo '$x = $y = 11 + 9 : ', var_dump($x = $y = 11 + 9), "</br>";
+        echo '($c % 2 == 0) ? "Es par" : "Es impar" : ', var_dump(($c % 2 == 0) ? "Es par" : "Es impar"), "</br>";
+        echo '($d) ?? "defecto" : ', var_dump(($d) ?? "defecto"), "</br>";
+        echo 'match($c) {$c % $a == 0 => "Divisible por $a", $c % $b => "Divisible por $b", default => "No divisible"} : ',
+        var_dump(match ($c) {$c % $a == 0 => "Divisible por $a", $c % $b => "Divisible por $b", default => "No divisible"}), "</br>";
         ?>
     </body>
 </html>
