5e824e42b317e64c57e752e9f9c689e8b1782978
Iván <jivanrod@gmail.com>
2022-10-25 22:49:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <h1>Evaluación de expresiones</h1>
         <?php
         $a = 5;
         $b = 10;
@@ -22,14 +23,15 @@ and open the template in the editor.
         $resultado = true and false;
         echo '$resultado = true and false : ', "Valor de resultado: ", var_dump($resultado), "</br>";
         echo '$resultado = ++$a : ', var_dump($resultado = ++$a), "</br>";
-        echo '$a == $b <= $c : ', var_dump($a == $b <= $c) , "</br>";
+        echo '$a == $b <= $c : ', var_dump($a == $b <= $c), "</br>";
         echo '$saludo .= "Hola " : ', var_dump($texto .= "Hola "), "</br>";
         echo '$x = ($y = 11)+9 : ', var_dump($x = ($y = 11) + 9), "</br>";
         echo '$x = $y = 11 + 9 : ', var_dump($x = $y = 11 + 9), "</br>";
         echo '($c % 2 == 0) ? "Es par" : "Es impar" : ', var_dump(($c % 2 == 0) ? "Es par" : "Es impar"), "</br>";
         echo '($d) ?? "defecto" : ', var_dump(($d) ?? "defecto"), "</br>";
         echo 'match($c) {$c % $a == 0 => "Divisible por $a", $c % $b => "Divisible por $b", default => "No divisible"} : ',
-        var_dump(match ($c) {$c % $a == 0 => "Divisible por $a", $c % $b => "Divisible por $b", default => "No divisible"}), "</br>";
+        var_dump(match ($c) {$c % $a == 0 => "Divisible por $a", $c % $b => "Divisible por $b", default => "No divisible"
+        }), "</br>";
         ?>
     </body>
 </html>
